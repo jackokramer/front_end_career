4eb190811dca6f1b12e4b6332cda2d9a2c1a6cdc
jackokramer <jackokramer@gmail.com>
2020-07-21 20:21:32 -0700

    tuezz

--- scrimba_react/src/searchMovies.js
+++ scrimba_react/src/searchMovies.js
@@ -1,28 +1,31 @@
-import React, {useState} from "react";
-
+import React, {useState} from "react"; // new hook in react for useState
+import MovieCard from './movieCard.js'
 export default function SearchMovies(){
 
     /// states - input query
     const [query, setQuery] = useState('')
 
-    const  searchMovies = async (e) =>{
-        e.preventDefault();
-        console.log(`submitting`)
+    // create the state for movies, and update the stat appropriately
+    const [movies, setMovies] = useState([])
 
+    const searchMovies = async (e) =>{
+        e.preventDefault();
         const query = 'Jurassic Parks';
 
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=a89842db3e5424c93c679acd490e840fab&language=en-US&query=${query}&page=1&include_adult=false`;
         
         try{
-        const res = await fetch(url);
-        const data = res.json();
-        console.log(data);
+            const res = await fetch(url);
+            const data = res.json();
+            console.log(data.results);
+            setMovies(data.results);
         } catch(err){
             console.log(err);
         }
     }
 
     return (
+        <>
         <form className="form">
             <label className="label" htmlFor="query">Movie Name</label>
             <input className="input" type="text" name="query"
@@ -31,5 +34,11 @@ export default function SearchMovies(){
                 />
             <button className="button" type="submit">Search</button>
         </form>
+        <div className="card-list"> 
+            {movies.filter(movie=> movie.poster_path).map(movie =>(
+                <MovieCard movie={movie} key={movie.id}/>
+            ))}
+        </div>
+        </>
     )
 }
