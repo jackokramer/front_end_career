3b1aea5831c91e434cd97d8bc61475fd47ce36e6
jackokramer <jackokramer@gmail.com>
2020-08-03 11:24:23 -0700

    in progress weather app

---
+++ jquery_vsreact/cards/my-app/components/WeatherData.js
@@ -0,0 +1,36 @@
+import React from `react`
+import WeatherDetails from './WeatherDetails'
+
+// extract out the following properties from this.props.weatherData
+
+//  main, name, sys, weather
+
+// replace the current static values in weatherData component with the appropriate values 
+
+// uncomment the image tag and display it after copying and pasting the weatherIcon constant
+
+// tip - you do not need to add the "$" sign when inject variables in JSX, just use curly braces.
+
+//<img className="weather-icon" src=${weatherIcon} alt="WeatherIcon"/>
+
+class WeatherData extends React.Component{
+    state = {
+        showWeatherDetails: false;
+    }
+    render() {
+    const {main, name, sys, weather} = this.props.WeatherData
+    const weatherIcon = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`
+    return (
+        <div className = "weather-data">
+            <h3 className="city-name">{name}, {sys.country}</h3>
+            <p className="weather-temperature">{main.temp}°C</p>
+            <img className="weather-icon" src={weatherIcon} alt="WeatherIcon"/>
+            <button onClick={()=> this.setState({showWeatherDetails: true})} 
+            className="weather-details-button">Details</button>
+            {this.state.showWeatherDetails && <WeatherDetails main={main}/>}
+        </div>
+    )
+    }
+}
+
+export default WeatherData
--- jquery_vsreact/cards/my-app/src/index.js
+++ jquery_vsreact/cards/my-app/src/index.js
@@ -3,23 +3,32 @@ import ReactDOM from 'react-dom';
 //import './index.css';
 import Header from "./components/Header"
 import WeatherSearch from "./components/WeatherSearch"
+import WeatherData from "./components/WeatherData"
+import WeatherDetails from '../components/WeatherDetails';
 //import App from './App';
 //import * as serviceWorker from './serviceWorker';
 
 
 class App extends React.Component {
-  api_call = async () =>{
-    const API_KEY = "9c3cb98520f309bd159e77512e8e5e28"
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}&units=metric`
-    const request = await fetch(url)
-    const response = await request.json()
-    console.log(`success!`)
+  state = {
+    weather: null
+  }
+  api_call = async (e) =>{
+      e.preventDefault()
+      const API_KEY = "9c3cb98520f309bd159e77512e8e5e28"
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}&units=metric`
+      const request = await fetch(url)
+      const response = await request.json()
+      // never directly manipulate the state like EX this.state.weather = response NOOOO
+      this.setState({weather: response})//instead 
   }
   render(){
     return(
       <>
         <Header />
-        <WeatherSearch />
+        <WeatherSearch api_call={this.api_call}/>
+        { this.state.weather && <WeatherData weatherData={this.state.weather}/>}
+        <WeatherDetails />
       </>
     )
   }
