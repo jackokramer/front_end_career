387f66a49a13fce01b90cf0efa353bfd6b56d179
jackokramer <jackokramer@gmail.com>
2020-08-10 17:28:03 -0700

    javascript basics refresher

---
+++ javascript_boot/index.js
@@ -0,0 +1,18 @@
+let name  = 'jack';
+
+name = "andy"
+
+console.log(`hi ` + name);
+
+/// cammel case = first word lowercase second first letter is capitalized
+
+// variable names only contain letters numbers, and symbol $ _
+// First character can't be a number or a - sign at the end
+
+var firstname = "Reed ";
+
+var surName = "Barger";
+
+var oneThousand = 1000;
+
+console.log(firstname + surName)
