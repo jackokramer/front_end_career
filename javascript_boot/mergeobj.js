53f72a99b5fc5c411dd2d0b9c4e25c1ad67db56b
jackokramer <jackokramer@gmail.com>
2020-09-01 20:18:36 -0700

    merge objects

---
+++ javascript_boot/mergeobj.js
@@ -0,0 +1,25 @@
+const user = {
+    name: '',
+    username: '',
+    phoneNumber: '',
+    email: '',
+    password: ''
+}
+
+const newUser = {
+    username: 'jackokramer',
+    email: 'reeeder@gmail.com',
+    password: 'myp$$word'
+}
+
+
+//const verifiedDefault = {
+//    verified: false;
+//}
+
+const createdUser = { ...user, newUser, verified: false};
+
+console.log(createdUser)
+
+//console.log(Object.assign({}, user, newUser, {verified: false}));//allows you to update an object with properties from another object.
+//console.log(user);
